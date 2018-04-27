7030a8de7d77097dd8323f6a357ce8b2f1c1a189
jimmy hu <60071jimmy@gmail.com>
2018-04-28 01:07:08 +0800

    Update ubuntu test
    Update ubuntu test

--- test/.archer.d/init.bash
+++ test/.archer.d/init.bash
@@ -1,7 +1,7 @@
 
 dotarcher_init() {
     archer_core_info "---- dotarcher_init begin ----"
-    ARCHER_LAYER_PREFIX=
+    ARCHER_LAYER_PREFIX=Ubuntu
     archer_core_info "---- dotarcher_init end ----"
     true
 }
